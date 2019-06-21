620e4b51fbbc54ae62afe46d2167ae674f8fec86
Harald Ringvold <harald.ringvold@gmail.com>
2019-06-22 01:06:01 +0200

    Fix stops controller

--- ui/lib/ui_web/controllers/stops_controller.ex
+++ ui/lib/ui_web/controllers/stops_controller.ex
@@ -1,8 +1,6 @@
 defmodule UiWeb.StopsController do
   use UiWeb, :controller
 
-  plug :action
-
   def index(conn, _params) do
   	stops = Application.fetch_env!(:ui, :stops)
     render conn, "index.json", stops: stops 
